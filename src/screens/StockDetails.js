1742def7c1747759316c28aa9755b8f9216d9c3d
Nidhi Deswal <nidhi.deswal@klearexpress.com>
2019-06-30 23:46:49 +0530

    added navigation between screens.

--- src/screens/StockDetails.js
+++ src/screens/StockDetails.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Text,View,Button} from 'react-native';
 
 export default class StockDetails extends React.Component{
+    goToStockFormScreen = () =>{
+        this.props.navigation.navigate('StockForm');
+    };
+
     render(){
         return(
-            <Text> Stock Details Screen </Text>
+            <View>
+                <Text> Stock Details Screen </Text>
+                <Button
+                    onPress={this.goToStockFormScreen}
+                    title="Go To Stock Form Screen"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
         )
     }
 }
